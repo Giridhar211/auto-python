77569502359cec90ff0b189072baa15574e6c962
simonduerr-coder <simon.duerr1996@gmail.cm>
2021-11-22 22:27:48 +0100

    add Get-Package Function to get Python Packages from or import statement

--- python_runner/python_runner.ps1
+++ python_runner/python_runner.ps1
@@ -1,14 +1,29 @@
 ﻿$PATH = "C:\tmp"
 $files = Get-ChildItem $PATH
 
-Write-Output $files
+function Get-Packages{
 
-foreach($file in $files){
-
-    $match = Select-String -InputObject $file -Pattern "from"  
-    $match | % { $match_string += "$_ "} #Var Type System Array
+    foreach($file in $files){
     
-    $pip_imports = ($match -replace "C:|tmp|$files|\\|\d|:|#|from|import", "")
-    Write-Output($pip_imports)
-}
+        #create Folder for Processing, delete it after usage in script
+        New-Item -Path C:\lightrunner -ItemType directory
+   
+        $match = Select-String -InputObject $file -Pattern "from"  
+        $match | % { $match_string += "$_ "} #Var Type System Array
 
+    
+        #Remove Illegal Chars and Numbers
+        $path_length = $PATH.Length
+        $regex_var = $match -replace ("^.{0,$path_length}|$file|:|\d","") | Out-File -Encoding ascii -Append C:\lightrunner\regex_py.json
+
+        #Get Packages
+        $lines_txt = Get-Content C:\lightrunner\regex_py.json
+        foreach($_ in $lines_txt){
+            $package = $_.split()
+            Write-Output $package[1]
+            #Safe Package Names in pip_packages_py.json file
+            $package[1] | Out-File -Encoding ascii -Append C:\lightrunner\pip_packages_py.json
+        }
+    }
+    #Remove-Item -Recurse -Force -Path C:\lightrunner
+}
