a9c6c50d60d69055369ecfdcf34670cc96d8964c
simonduerr-coder <simon.duerr1996@gmail.cm>
2021-11-22 15:16:26 +0100

    add python runner.ps1

---
+++ python_runner/python_runner.ps1
@@ -0,0 +1,14 @@
+﻿$PATH = "C:\tmp"
+$files = Get-ChildItem $PATH
+
+Write-Output $files
+
+foreach($file in $files){
+
+    $match = Select-String -InputObject $file -Pattern "from"  
+    $match | % { $match_string += "$_ "} #Var Type System Array
+    
+    $pip_imports = ($match -replace "C:|tmp|$files|\\|\d|:|#|from|import", "")
+    Write-Output($pip_imports)
+}
+
